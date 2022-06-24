a615f38420e43870deacf25663590ccb2cbc6de4
denysatriaardi <86131172+denysat25@users.noreply.github.com>
2022-06-24 21:44:38 +0700

    Add files via upload

---
+++ Installation Laravel/pintegratif/storage/framework/views/cb8b656761e581548086dfab01888dfb95b1b132.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Halaman HTML</h1>
+
+<?php
+echo "Halo dunia!";
+?>
+
+</body>
+</html><?php /**PATH C:\Users\deny\Documents\Laravel_App\pintegratif\resources\views/hal1.blade.php ENDPATH**/ ?>
